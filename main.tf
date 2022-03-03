d7ad4a03107a339d1f29169f0851655533ea7d39
Eugenio Duarte <eduarte@Eugenios-MacBook-Pro.local>
2022-03-03 15:05:14 -0500

    adding specific aws provider version

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 
 # TERRAFORM CONFIGURATION FOR AWS
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+}
 
 provider "aws" {
   region = var.aws_region
